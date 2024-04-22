bea2ceb895ce3b8a3cc9c8cdb5f71f987b9ad26b
jpham9945 <90347564+jpham9945@users.noreply.github.com>
2024-04-22 16:43:14 -0700

    first commit

---
+++ app/components/HeroSection.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>      
+        <h1 className="text-white mb-4 text-4xl font-extrabold">
+            Hello, I'm James
+        </h1>
+        <p className="text-stone-200 text-lg lg:text-xl">
+            LORD THIS BITCH TYPES FAST IM GONNA FUCKING SHOOT MYSELF IF THIS HOE DONT STOP MOVING THE FUCKING SCREEN AROUND 
+        </p>
+        
+    </section>
+  )
+}
+
+export default HeroSection
+
